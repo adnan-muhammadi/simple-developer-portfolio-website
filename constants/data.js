01f38927643f208f745e7ca982abc201f4a796c6
Muhammad Adnan <madnan@DESKTOP-SGF0001.abacusoft.com>
2023-01-04 13:27:44 +0500

    first commit

---
+++ constants/data.js
@@ -0,0 +1,136 @@
+const userData = {
+  githubUsername: "adnan-muhammadi",
+  name: "Muhammad Adnan",
+  designation: "Frontend Developer/ Software Engineer",
+  avatarUrl: "/avatar.png",
+  email: "adnan.jmuhammadi@gmail.com",
+  phone: "+92 3177912895",
+  address: "Karachi, Pakistan.",
+  projects: [
+    {
+      title: "BG Products",
+      link: "https://bgprod.com",
+      imgUrl: "/tmk2.jpg",
+    },
+    {
+      title: "TopTech TMS",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.png",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "My Resume",
+      link: "https://drive.google.com/file/d/1tx0jR-pgmXTVekq3CvLBzLSgjaC0TEz-/view?usp=share_link",
+      imgUrl: "/me.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Node.js, Express, MongoDB, MySQL, Bootstrap, Material UI, Kendo UI, PHP, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "BG Products",
+    currentProjectUrl: "https://bgprod.com",
+  },
+  experience: [
+    {
+      title: "Software Engineer",
+      company: "Abacusoft",
+      year: "2022-Present",
+      companyLink: "https://thetechresources.com",
+      desc: "Contributed to their flagship product - BG Products Inc. - which is committed to maintaining vehicles through high quality automotive maintenance services for fuel systems, engines, transmissions, brakes, power steering, cooling, battery, driveline, and climate control systems.",
+    },
+    {
+      title: "Senior Frontend Developer",
+      company: "MayaBytes",
+      year: "2021-2022",
+      companyLink: "https://mayabytes.com",
+      desc: "Developed a marketplace for Lone Wolf Project (React, HTML and JavaScript).",
+    },
+    {
+      title: "Senior Frontend Developer",
+      company: "GadiTek",
+      year: "2020 - 2021",
+      companyLink: "https://www.gaditek.com/",
+      desc: "Contributed to Stretto",
+    },
+    {
+      title: "Frontend Developer",
+      company: "ChaiOne",
+      year: "2019 - 2020",
+      companyLink: "https://www.chaione.com",
+      desc: "Contributed to our flagship Toptech Terminal Management System",
+    },
+    {
+      title: "Frontend Developer",
+      company: "Techsurge",
+      year: "2017 - 2019",
+      companyLink: "https://www.techsurgeinc.com/",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Internship",
+      company: "Valley Labs",
+      year: "2017",
+      companyLink: "https://www.indusvalleylabs.com/",
+      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+    },
+    {
+      title: "Graduation",
+      company: "IQRA University",
+      year: "2022",
+      companyLink: "https://www.iqra.edu.pk/",
+      desc: "Major in Computer Science and Engineering with a CGPA of 3.3. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "DJ Science College, Karachi.",
+      year: "2018",
+      companyLink: "https://www.facebook.com/dayaramjaethmalcollege/",
+      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1tx0jR-pgmXTVekq3CvLBzLSgjaC0TEz-/view?usp=share_link",
+  socialLinks: {
+    instagram: "https://instagram.com/adnan.muhammadi",
+    twitter: "https://twitter.com/adnan-muhamadi",
+    linkedin: "https://linkedin.com/in/theadnan",
+    github: "https://github.com/adnan-muhamadi",
+    facebook: "https://facebook.com/adnan.joiya.muhammadi",
+  },
+};
+
+export default userData;

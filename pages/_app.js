bde21e2ef2b2d37d7eadef30828eb66e8eff21ef
Aisha <aishasaleem8844@gmail.com>
2023-01-08 17:21:04 +0500

    analystics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider defaultTheme="dark" attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider defaultTheme="dark" attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <Analytics />
+    </>
   );
 }
-
-export default MyApp;
